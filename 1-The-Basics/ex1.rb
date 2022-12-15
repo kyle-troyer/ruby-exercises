7505df30e5549b923cfa5f90a2f6298cc882fa75
Kyle Troyer <ktfinzer@gmail.com>
2022-12-15 01:52:45 +0000

    Update exercise files

---
+++ 1-The-Basics/ex1.rb
@@ -0,0 +1,2 @@
+full_name = "Kyle " + "Troyer"
+puts full_name
---
+++ 1-The-Basics/ex2.rb
@@ -0,0 +1,8 @@
+number = 5673
+
+puts "Digit in thousands place is: " + (number/1000).to_s
+hundreds = number % 1000
+puts "Digit in hundres place is: " + (hundreds/100).to_s
+tens = number % 100
+puts "Digit in tens place is: " + (tens/10).to_s
+puts "Digit in ones place is: " + (number % 10).to_s
